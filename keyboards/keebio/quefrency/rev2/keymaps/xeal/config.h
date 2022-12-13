0a0fd232781791c3c7f614c7666b2a2fbbedd8fc
lund-tech <lund.spencer.22@gmail.com>
2022-12-13 11:30:37 -0800

    updating with qufrency files

--- keyboards/keebio/quefrency/rev2/keymaps/xeal/config.h
+++ keyboards/keebio/quefrency/rev2/keymaps/xeal/config.h
@@ -4,6 +4,7 @@ This is the c configuration file for the keymap
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
 Copyright 2018 Danny Nguyen <danny@keeb.io>
+Copyright 2021 Roger Billerey-Mosier
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -21,4 +22,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// #define USE_I2C
+/* Use I2C or Serial, not both */
+
+//#define DEBUG_MATRIX_SCAN_RATE
+
+#define FORCE_NKRO
+
+#undef RGBLIGHT_EFFECT_SNAKE
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#undef RGBLIGHT_EFFECT_RGB_TEST
+#undef RGBLIGHT_EFFECT_ALTERNATING
+#undef RGBLIGHT_EFFECT_TWINKLE
+
+#define LAYER_STATE_8BIT
+#define MAX_LAYER 3
